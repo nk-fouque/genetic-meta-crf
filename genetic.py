f2e31643461d376faef37127c0520ed1c1a6cfd9
reblochor <rdahuron@insa-rennes.fr>
2020-02-13 14:39:10 +0100

    initial commit

---
+++ genetic.py
@@ -0,0 +1,60 @@
+import random
+from collections import namedtuple
+from typing import Tuple, List
+
+POPULATION_SIZE = 20
+
+Rule = namedtuple("Rule", "arity param1 param2")
+
+
+def mutate_rule(rule: Rule):
+    pass
+
+
+def add_random_rule(individual: List[Rule]):
+    pass
+
+
+def mutate(individual: List[Rule]) -> None:
+    for rule in individual:
+        mutate_rule(rule)
+    if random.randint(0, 100) < 10:
+        add_random_rule(individual)
+
+
+def cross_individuals(i1: list, i2: list) -> Tuple[List, List]:
+    total = i1 + i2
+    random.shuffle(total)
+    cut = random.randint(1, len(total))
+    return total[:cut], total[cut:]
+
+
+def create_rule() -> Rule:
+    arity = 'U' if random.randint(0, 5) < 3 else 'B'
+    param1 = random.randint(-2, 2)
+    param2 = 0
+    return Rule(arity, param1, param2)
+
+
+def create_individual() -> List[Rule]:
+    size = random.randint(1, 6)
+    return [create_rule() for x in range(size)]
+
+
+def initialize_population() -> List[List[Rule]]:
+    return [create_individual() for x in range(POPULATION_SIZE)]
+
+
+if __name__ == '__main__':
+    pop = initialize_population()
+    print(pop)
+
+    crossed_pop = []
+    while len(crossed_pop) < POPULATION_SIZE:
+        random.shuffle(pop)
+        for i1, i2 in zip(pop[:2], pop[2::4]):
+            a, b = cross_individuals(i1, i2)
+            crossed_pop.append(a)
+            crossed_pop.append(b)
+    print(crossed_pop)
+    print(len(crossed_pop))
