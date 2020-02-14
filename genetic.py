3ca7c6e5fc5e5d04bb3b0e473d37fb01e81ff31c
nk-fouque <fouquen@outlook.fr>
2020-02-14 11:17:49 +0100

    Created mutation rules

--- genetic.py
+++ genetic.py
@@ -3,22 +3,47 @@ from collections import namedtuple
 from typing import Tuple, List
 
 POPULATION_SIZE = 20
+RANDOM_RULE_CHANCE = 10
+MUTATE_ARITY_CHANCE = 30
+MUTATE_PARAM_CHANCE = 50
+MUTATE_DOUBLE_CHANCE = 20
 
-Rule = namedtuple("Rule", "arity param1 param2")
+
+Rule = namedtuple("Rule", "arity param1 param2 param3 param4")
 
 
 def mutate_rule(rule: Rule):
-    pass
+    mutation = random.randint(0,100)
+    arity = getattr(rule,'arity')
+    param1 = getattr(rule,'param1')
+    param2 = getattr(rule,'param2')
+    param3 = getattr(rule,'param3')
+    param4 = getattr(rule,'param4')
+    if mutation < MUTATE_ARITY_CHANCE:
+        if arity == 'U':
+            arity = 'B'
+        if arity == 'B':
+            arity = '*'
+        if arity == '*':
+            arity = 'U'
+    if mutation >= MUTATE_ARITY_CHANCE & mutation < MUTATE_PARAM_CHANCE:
+            param1+=random.randint(-1,1)
+            param2+=random.randint(-1,1)
+    if mutation >= MUTATE_ARITY_CHANCE+MUTATE_PARAM_CHANCE & mutation <MUTATE_DOUBLE_CHANCE:
+            param3 = param1
+            param4 = 0
+    return Rule(arity,param1,param2,param3,param4)
 
 
 def add_random_rule(individual: List[Rule]):
+    individual+=(create_rule())
     pass
 
 
 def mutate(individual: List[Rule]) -> None:
     for rule in individual:
         mutate_rule(rule)
-    if random.randint(0, 100) < 10:
+    if random.randint(0, 100) < RANDOM_RULE_CHANCE:
         add_random_rule(individual)
 
 
@@ -30,10 +55,10 @@ def cross_individuals(i1: list, i2: list) -> Tuple[List, List]:
 
 
 def create_rule() -> Rule:
-    arity = 'U' if random.randint(0, 5) < 3 else 'B'
+    arity = 'U' if random.randint(0, 5) < 3 else 'B' if random.randint(0,5) < 3 else '*'
     param1 = random.randint(-2, 2)
     param2 = 0
-    return Rule(arity, param1, param2)
+    return Rule(arity, param1, param2, None, None)
 
 
 def create_individual() -> List[Rule]:
