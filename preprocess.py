4f8eb77b3c002e54b3f35285ac94ad50b128aebb
nk-fouque <fouquen@outlook.fr>
2020-05-06 13:50:17 +0200

    Implemented cities in preprocessing

--- preprocess.py
+++ preprocess.py
@@ -11,13 +11,17 @@ if __name__ == '__main__':
     input = sys.argv[1]
     output = sys.argv[2]
     states = read_words('preprocessing_data/state')
+    cities = read_words('preprocessing_data/cities')
 
     with open(input, 'r') as file:
         filedata = file.read()
 
     # Replace the target string
     for state in states:
-        filedata[:] = [s.replace(state+'\t', 'state-token\t') for s in filedata]
+        filedata = filedata.replace(state+'\t', 'state-token\t')
+    for city in cities:
+        filedata = filedata.replace(city+'\t', 'city-token\t')
+        filedata = filedata.replace(city+'-airport\t', 'city-airport-token\t')
 
     # Write the file out again
     with open(output, 'w') as file:
