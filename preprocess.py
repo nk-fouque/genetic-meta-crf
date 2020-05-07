41eac04d4122f8421c07b5a0c90772a7ec67d8ad
nk-fouque <fouquen@outlook.fr>
2020-05-07 13:13:33 +0200

    Implemented days in preprocessing

--- preprocess.py
+++ preprocess.py
@@ -12,16 +12,19 @@ if __name__ == '__main__':
     output = sys.argv[2]
     states = read_words('preprocessing_data/state')
     cities = read_words('preprocessing_data/cities')
+    days = read_words('preprocessing_data/days')
 
     with open(input, 'r') as file:
         filedata = file.read()
 
     # Replace the target string
     for state in states:
-        filedata = filedata.replace(state+'\t', 'state-token\t')
+        filedata = filedata.replace(state+'\n', 'state-token\n')
     for city in cities:
-        filedata = filedata.replace(city+'\t', 'city-token\t')
-        filedata = filedata.replace(city+'-airport\t', 'city-airport-token\t')
+        filedata = filedata.replace(city+'\n', 'city-token\n')
+        filedata = filedata.replace(city+'-airport\n', 'city-airport-token\n')
+    for day in days:
+        filedata = filedata.replace(day+'\n', 'day-token\n')
 
     # Write the file out again
     with open(output, 'w') as file:
